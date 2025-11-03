7c9d4bfe1c25b07df3d918c7dbd0c626247a295a
Alireza Shirzad <alr.shirzad@gmail.com>
2025-11-03 18:03:32 -0500

    Revert "Refactor sort tests and improve activator handling in sort functions"
    This reverts commit ecf0d6a60f1948c42ae829e3de391390da9d49e5.

--- exec/tests/sort.rs
+++ exec/tests/sort.rs
@@ -5,11 +5,11 @@ use support::end_to_end_tests;
 use truthtable_core::test_display::display_prover_hint_tree;
 
 end_to_end_tests!(&["lineitem"] => [
-    // project_sort => r#"
-    //     SELECT l_suppkey
-    //     FROM lineitem
-    //     ORDER BY l_suppkey ASC;
-    // "#,
+    project_sort => r#"
+        SELECT l_suppkey
+        FROM lineitem
+        ORDER BY l_suppkey ASC;
+    "#,
     project_sort_1 => r#"
 SELECT 
     l_suppkey,
@@ -24,20 +24,6 @@ ORDER BY 4 + (l_suppkey * 7 + 3) DESC, l_suppkey ASC;
 FROM lineitem
 WHERE l_suppkey > 1000
 ORDER BY 4 +  (l_suppkey * 7 + 3) DESC, l_suppkey ASC;"#,
-    groupby_sort => r#"
-SELECT
-    l_returnflag,
-    l_linestatus,
-    SUM(l_extendedprice * (1 - l_discount)) AS revenue,
-    COUNT(*) AS row_count
-FROM lineitem
-GROUP BY
-    l_returnflag,
-    l_linestatus
-ORDER BY
-    l_returnflag,
-    l_linestatus;
-    "#,
 ]);
 
 type F = ark_test_curves::bls12_381::Fr;
