b32ca57d719ea56e60f92e07fdf73f570be5fe60
Alireza Shirzad <alrshir@seas.upenn.edu>
2026-01-30 16:28:49 -0500

    feat: add OptimizeProjections rule to eliminate unused columns in logical plans

--- proof-planner/src/logical_plan_optimizer/mod.rs
+++ proof-planner/src/logical_plan_optimizer/mod.rs
@@ -4,20 +4,23 @@ use datafusion::{
     optimizer::{
         common_subexpr_eliminate::CommonSubexprEliminate, eliminate_cross_join::EliminateCrossJoin,
         eliminate_duplicated_expr::EliminateDuplicatedExpr, eliminate_filter::EliminateFilter,
-        eliminate_join::EliminateJoin, eliminate_limit::EliminateLimit,
+        eliminate_group_by_constant::EliminateGroupByConstant, eliminate_join::EliminateJoin,
+        eliminate_limit::EliminateLimit, eliminate_nested_union::EliminateNestedUnion,
+        eliminate_one_union::EliminateOneUnion,
         extract_equijoin_predicate::ExtractEquijoinPredicate,
         optimize_projections::OptimizeProjections,
         propagate_empty_relation::PropagateEmptyRelation, push_down_filter::PushDownFilter,
-        push_down_limit::PushDownLimit, simplify_expressions::SimplifyExpressions, OptimizerRule,
+        push_down_limit::PushDownLimit, scalar_subquery_to_join::ScalarSubqueryToJoin,
+        simplify_expressions::SimplifyExpressions, OptimizerRule,
     },
     prelude::SessionContext,
 };
 
+mod customized_optimize_projections;
 mod lift_join_filter;
 mod merge_filters;
 mod normalize_table_scan;
 mod rematerialize;
-
 // pub(crate) fn optimize_logical_plan(plan: LogicalPlan) -> LogicalPlan {
 //     let rules: Vec<Arc<dyn OptimizerRule + Send + Sync>> = vec![
 //         Arc::new(ExtractEquijoinPredicate),
@@ -34,8 +37,10 @@ mod rematerialize;
 
 pub fn rules(session_ctx: &SessionContext) -> Vec<Arc<dyn OptimizerRule + Send + Sync>> {
     vec![
+        Arc::new(EliminateNestedUnion::new()),
         Arc::new(SimplifyExpressions::new()),
         Arc::new(EliminateJoin::new()),
+        Arc::new(ScalarSubqueryToJoin::new()),
         Arc::new(ExtractEquijoinPredicate::new()),
         Arc::new(EliminateDuplicatedExpr::new()),
         Arc::new(EliminateFilter::new()),
@@ -44,16 +49,19 @@ pub fn rules(session_ctx: &SessionContext) -> Vec<Arc<dyn OptimizerRule + Send +
         Arc::new(EliminateLimit::new()),
         Arc::new(PropagateEmptyRelation::new()),
         // Must be after PropagateEmptyRelation
-        ///////
+        Arc::new(EliminateOneUnion::new()),
         // Filters can't be pushed down past Limits, we should do PushDownFilter after PushDownLimit
         Arc::new(PushDownLimit::new()),
         Arc::new(PushDownFilter::new()),
         // The previous optimizations added expressions and projections,
         // that might benefit from the following rules
         Arc::new(SimplifyExpressions::new()),
+        Arc::new(CommonSubexprEliminate::new()),
+        Arc::new(EliminateGroupByConstant::new()),
         Arc::new(normalize_table_scan::NormalizeTableScanPushdown::new()),
         Arc::new(merge_filters::MergeConsecutiveFilters::new()),
         Arc::new(lift_join_filter::LiftJoinFilter::new()),
-        // Arc::new(rematerialize::RematerializeRule::new(session_ctx.state())),
+        Arc::new(customized_optimize_projections::OptimizeProjections::new()),
+        Arc::new(rematerialize::RematerializeRule::new(session_ctx.state())),
     ]
 }
