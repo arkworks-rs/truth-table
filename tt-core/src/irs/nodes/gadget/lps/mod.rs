3946b603cb70270c30ea0555b7fd42f28b8bb4d1
Alireza Shirzad <alrshir@seas.upenn.edu>
2026-02-03 14:26:57 -0500

    feat: add one-to-one join gadget node implementation

--- tt-core/src/irs/nodes/gadget/lps/mod.rs
+++ tt-core/src/irs/nodes/gadget/lps/mod.rs
@@ -2,4 +2,5 @@ pub mod aggregate;
 pub mod filter;
 pub mod join;
 pub mod limit;
+pub mod one_to_one_join;
 pub mod sort;
---
+++ tt-core/src/irs/nodes/gadget/lps/one_to_one_join/mod.rs
@@ -0,0 +1,119 @@
+use crate::irs::nodes::{IsGadgetNode, IsNode, Node, ProverNodeOps, VerifierNodeOps};
+use crate::prover::irs::GadgetReadyIr;
+use crate::verifier::irs::GadgetReadyIr as VerifierGadgetReadyIr;
+use ark_piop::SnarkBackend;
+use datafusion_expr::Join;
+use indexmap::IndexMap;
+
+pub struct GadgetNode<B: SnarkBackend> {
+    phantom: std::marker::PhantomData<B>,
+}
+
+impl<B: SnarkBackend> IsNode<B> for GadgetNode<B> {
+    fn name(&self) -> String {
+        "Join".to_string()
+    }
+
+    fn display(&self) -> String {
+        let name = self.name();
+        crate::irs::nodes::display_with_inputs(&name, &self.children())
+    }
+
+    fn cost(
+        &self,
+        _statistics: datafusion_common::Statistics,
+        _schema: arrow_schema::SchemaRef,
+    ) -> crate::irs::nodes::cost::ProvingCost {
+        todo!()
+    }
+
+    fn initialize_gadget_plans(
+        &self,
+        id: crate::irs::nodes::NodeId,
+        planned_ir: &mut crate::irs::shared_ir::OutputPlannedIr<B>,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        todo!()
+    }
+
+    fn children(&self) -> Vec<std::sync::Arc<Node<B>>> {
+        vec![]
+    }
+}
+
+impl<B: SnarkBackend> ProverNodeOps<B> for GadgetNode<B> {
+    fn add_virtual_witness(
+        &self,
+        _id: crate::irs::nodes::NodeId,
+        _virtualized_ir: &mut crate::prover::irs::VirtualizedIr<B>,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        Ok(())
+    }
+
+    fn initialize_gadgets(
+        &self,
+        id: crate::irs::nodes::NodeId,
+        _prover: &mut ark_piop::prover::ArgProver<B>,
+        virtualized_ir: &mut crate::prover::irs::VirtualizedIr<B>,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        todo!()
+    }
+}
+
+impl<B: SnarkBackend> VerifierNodeOps<B> for GadgetNode<B> {
+    fn add_virtual_witness(
+        &self,
+        _id: crate::irs::nodes::NodeId,
+        _virtualized_ir: &mut crate::verifier::irs::VirtualizedIr<B>,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        Ok(())
+    }
+    fn initialize_gadgets(
+        &self,
+        id: crate::irs::nodes::NodeId,
+        _verifier: &mut ark_piop::verifier::ArgVerifier<B>,
+        virtualized_ir: &mut crate::verifier::irs::VirtualizedIr<B>,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        todo!()
+    }
+}
+
+impl<B: SnarkBackend> IsGadgetNode<B> for GadgetNode<B> {
+    fn prove(
+        &self,
+        prover: &mut ark_piop::prover::ArgProver<B>,
+        gadget_ready_ir: &mut GadgetReadyIr<B>,
+        id: crate::irs::nodes::NodeId,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        todo!()
+    }
+
+    fn honest_prover_check(
+        &self,
+        _prover: &mut ark_piop::prover::ArgProver<B>,
+        _gadget_ready_ir: &mut GadgetReadyIr<B>,
+        _id: crate::irs::nodes::NodeId,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        Ok(())
+    }
+
+    fn verify(
+        &self,
+        verifier: &mut ark_piop::verifier::ArgVerifier<B>,
+        gadget_ready_ir: &mut VerifierGadgetReadyIr<B>,
+        id: crate::irs::nodes::NodeId,
+    ) -> ark_piop::errors::SnarkResult<()> {
+        todo!()
+    }
+
+    fn hints(&self) -> indexmap::IndexMap<String, crate::irs::nodes::hints::HintDF> {
+        IndexMap::new()
+    }
+}
+
+impl<B: SnarkBackend> GadgetNode<B> {
+    pub fn new(join: Join) -> Self {
+        Self {
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
