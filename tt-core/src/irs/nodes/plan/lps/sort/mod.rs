d2ab620c2f39ca7684b51e5f83b738d633510da4
Alireza Shirzad <alrshir@seas.upenn.edu>
2026-02-24 10:54:19 -0500

    feat: implement output method for verifier plan node in LpNode

--- tt-core/src/irs/nodes/plan/lps/sort/mod.rs
+++ tt-core/src/irs/nodes/plan/lps/sort/mod.rs
@@ -258,7 +258,8 @@ impl<B: SnarkBackend> VerifierNodeOps<B> for LpNode<B> {
         _id: NodeId,
         planned_ir: &mut crate::verifier::irs::OutputPlannedIr<B>,
     ) -> ark_piop::errors::SnarkResult<()> {
-        todo!()
+        let _ = planned_ir;
+        Ok(())
     }
 }
 
@@ -504,7 +505,29 @@ impl<B: SnarkBackend> crate::irs::nodes::IsProverPlanNode<B> for LpNode<B> {
 
 impl<B: SnarkBackend> crate::irs::nodes::IsVerifierPlanNode<B> for LpNode<B> {
     fn output(&self) -> VerifierHint {
-        todo!()
+        let prover_hint = <Self as crate::irs::nodes::IsProverPlanNode<B>>::output(self);
+        let schema = std::sync::Arc::new(
+            <datafusion_common::DFSchema as AsRef<datafusion::arrow::datatypes::Schema>>::as_ref(
+                prover_hint.data_frame().schema(),
+            )
+            .clone(),
+        );
+        let field_materialization = prover_hint
+            .field_materialization_iter()
+            .map(|(field, mat)| (field.clone(), *mat))
+            .collect::<indexmap::IndexMap<_, _>>();
+
+        let input_log_size = match self.input.as_ref() {
+            Node::Plan(plan_node) => {
+                <crate::irs::nodes::PlanNode<B> as crate::irs::nodes::IsVerifierPlanNode<
+                    B,
+                >>::output(plan_node)
+                .log_size()
+            }
+            Node::Gadget(_) => 0,
+        };
+
+        VerifierHint::from_field_materialization(schema, field_materialization, input_log_size)
     }
 }
 
